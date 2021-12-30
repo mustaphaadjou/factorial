ff37b8d8f0f1070faea5f6159df7ff20de05f75b
mustaphaadjou <59447547+mustaphaadjou@users.noreply.github.com>
2021-12-30 23:54:55 +0100

    Update factorial.cpp

--- factorial.cpp
+++ factorial.cpp
@@ -1,16 +1,22 @@
 #include<stdio.h>
 
 int main(){
+	//variables initialisation
 	int i,n,f;
+	//reading the variable from the keyboard
 	do{
 	printf("donner un nombre n positive:");
-	scanf("%d",&n);}while(n<0);
+	scanf("%d",&n);}
+	while(n<0);
+	//variables initialisation
 	f=1;
 	i=n;
+	//while loop to caluculate the factorial
 	while(i!=0){
 		f=f*i;
 		i--;
 	}
+	//printing the code in the screen
 	printf("%d",f);
 	return 0;
 }
